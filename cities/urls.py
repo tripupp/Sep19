e530459d77a8060db4341bf81c1e122489c07aa1
Devansh Ahuja <ahuja.devansh2@gmail.com>
2019-09-14 19:59:58 +0530

    all templates of cities are created

--- cities/urls.py
+++ cities/urls.py
@@ -2,6 +2,35 @@ from django.urls import path
 from . import views as city_views
 
 urlpatterns = [
-    path('<int:pk>-<str:slug>/', city_views.CityDetailView.as_view(),
-         name="cities-city_detail"),
+     path('<int:pk>-<str:slug>/', city_views.CityDetailView.as_view(),
+          name="cities-city_detail"),
+
+     path('<int:pk>-<str:slug>/places/', city_views.CityPlaceView.as_view(),
+          name="cities-city_place"),
+
+     path('<int:pk>-<str:slug>/packages/',
+          city_views.CityPackageView.as_view(),
+          name="cities-city_packages"),
+
+     path('<int:pk>-<str:slug>/travel/', city_views.CityTravelView.as_view(),
+          name="cities-city_tarvel"),
+
+     path('<int:pk>-<str:slug>/hotels/', city_views.CityHotelView.as_view(),
+          name="cities-city_hotels"),
+
+     path('<int:pk>-<str:slug>/Activities/',
+          city_views.CityActivityView.as_view(),
+          name="cities-city_activities"),
+
+     path('<int:pk>-<str:slug>/Events/', city_views.CityEventsView.as_view(),
+          name="cities-city_events"),
+
+     path('<int:pk>-<str:slug>/foods/', city_views.CityFoodView.as_view(),
+          name="cities-city_foods"),
+
+     path('<int:pk>-<str:slug>/photos/', city_views.CityPhotoView.as_view(),
+          name="cities-city_photo"),
+
+     path('<int:pk>-<str:slug>/map/', city_views.CityMapView.as_view(),
+          name="cities-city_map"),
 ]
--- cities/views.py
+++ cities/views.py
@@ -17,3 +17,121 @@ class CityDetailView(View):
             'city': city
         }
         return render(request, self.template_name, context)
+
+
+class CityPlaceView(View):
+
+    template_name = 'cities/places.html'
+    query_pk_and_slug = True
+
+    def get(self, request, *args, **kwargs):
+        places = Place.objects.filter(city=self.kwargs.get('pk'))
+        city = get_object_or_404(City, pk=self.kwargs.get('pk'))
+        context = {
+            'places': places,
+            'city': city
+        }
+        return render(request, self.template_name, context)
+
+class CityPackageView(View):
+
+    template_name = 'cities/package.html'
+    query_pk_and_slug = True
+
+    def get(self, request, *args, **kwargs):
+        city = get_object_or_404(City, pk=self.kwargs.get('pk'))
+        context = {
+            'city': city
+        }
+        return render(request, self.template_name, context)
+
+
+class CityTravelView(View):
+
+    template_name = 'cities/travel.html'
+    query_pk_and_slug = True
+
+    def get(self, request, *args, **kwargs):
+        city = get_object_or_404(City, pk=self.kwargs.get('pk'))
+        context = {
+            'city': city
+        }
+        return render(request, self.template_name, context)
+
+
+class CityHotelView(View):
+
+    template_name = 'cities/hotel.html'
+    query_pk_and_slug = True
+
+    def get(self, request, *args, **kwargs):
+        city = get_object_or_404(City, pk=self.kwargs.get('pk'))
+        context = {
+            'city': city
+        }
+        return render(request, self.template_name, context)
+
+
+class CityActivityView(View):
+
+    template_name = 'cities/activities.html'
+    query_pk_and_slug = True
+
+    def get(self, request, *args, **kwargs):
+        city = get_object_or_404(City, pk=self.kwargs.get('pk'))
+        context = {
+            'city': city
+        }
+        return render(request, self.template_name, context)
+
+
+class CityEventsView(View):
+
+    template_name = 'cities/events.html'
+    query_pk_and_slug = True
+
+    def get(self, request, *args, **kwargs):
+        city = get_object_or_404(City, pk=self.kwargs.get('pk'))
+        context = {
+            'city': city
+        }
+        return render(request, self.template_name, context)
+
+
+class CityFoodView(View):
+
+    template_name = 'cities/food.html'
+    query_pk_and_slug = True
+
+    def get(self, request, *args, **kwargs):
+        city = get_object_or_404(City, pk=self.kwargs.get('pk'))
+        context = {
+            'city': city
+        }
+        return render(request, self.template_name, context)
+
+
+class CityPhotoView(View):
+
+    template_name = 'cities/photos.html'
+    query_pk_and_slug = True
+
+    def get(self, request, *args, **kwargs):
+        city = get_object_or_404(City, pk=self.kwargs.get('pk'))
+        context = {
+            'city': city
+        }
+        return render(request, self.template_name, context)
+
+
+class CityMapView(View):
+
+    template_name = 'cities/map.html'
+    query_pk_and_slug = True
+
+    def get(self, request, *args, **kwargs):
+        city = get_object_or_404(City, pk=self.kwargs.get('pk'))
+        context = {
+            'city': city
+        }
+        return render(request, self.template_name, context)
